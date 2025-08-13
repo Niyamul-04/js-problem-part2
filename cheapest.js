ce75fdba564bf07444a5379782ccd2cc577fa870
Niyamul Hasan <niyamulhasan301@gmail.com>
2025-08-13 17:07:07 +0600

    Added discount.js

--- cheapest.js
+++ cheapest.js
@@ -1,29 +1,32 @@
-// const phones = [
-//     {name : 'samsung', price : 20000, camera: '12mp', color: 'black'},
-//     {name : 'xiomi', price : 200000, camera: '12mp', color: 'black'},
-//     {name : 'iphone', price : 2000000, camera: '12mp', color: 'black'},
-//     {name : 'daamsung', price : 2000000, camera: '12mp', color: 'black'},
-//     {name : 'walton', price : 2000, camera: '12mp', color: 'black'}
-// ]
-
-// function getCheapestPhone(phones){
-//     let min = phones [0];
-//     for(const phone of phones){
-//         if(phone.price < min.price){
-//             min = phone
-//         }
-//     }
-//     return min;
-
-
-// }
-
-// const cheap = getCheapestPhone(phones);
-// console.log('cheapest phone is:', cheap);
-
-
-
 const phones = [
+    {name : 'samsung', price : 20000, camera: '12mp', color: 'black'},
+    {name : 'xiomi', price : 200000, camera: '12mp', color: 'black'},
+    {name : 'iphone', price : 2000000, camera: '12mp', color: 'black'},
+    {name : 'daamsung', price : 2000000, camera: '12mp', color: 'black'},
+    {name : 'walton', price : 2000, camera: '12mp', color: 'black'}
+]
+
+function getCheapestPhone(phones){
+    let min = phones [0];
+    for(const phone of phones){
+        if(phone.price < min.price){
+            min = phone
+        }
+    }
+    return min;
+
+
+}
+
+const cheap = getCheapestPhone(phones);
+console.log('cheapest phone is:', cheap);
+
+
+
+
+
+
+const phones1 = [
     {name : 'samsung', price : 20000, camera: '12mp', color: 'black'},
     {name : 'xiomi', price : 200000, camera: '12mp', color: 'black'},
     {name : 'iphone', price : 2000000, camera: '12mp', color: 'black'},
@@ -42,5 +45,5 @@ function getMaxPhone(phones){
     return max;
 }
 
-const dami = getMaxPhone(phones);
+const dami = getMaxPhone(phones1);
 console.log('dami phone is:', dami);
